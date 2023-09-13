94dae2b7a40f4c94c4a2d5a4224987098866ae9d
Dzerdevwarnd <dzerdevwarnd3@gmail.com>
2023-09-13 17:57:54 +0300

    Rest get

--- dist/index.js
+++ dist/index.js
@@ -5,10 +5,29 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-const port = 3000;
-app.get('/', (req, res) => {
-    let helloMessage = 'Hello11 Wor ld!';
-    res.send(helloMessage);
+const port = process.env.PORT || 3000;
+const products = [
+    { id: 1, title: 'tomato' },
+    { id: 2, title: 'orange' },
+];
+const adresses = [{ value: 'Nezalejnasti 12' }, { value: 'Selickaga 11' }];
+app.get('/products', (req, res) => {
+    if (req.query.title) {
+        let searchString = req.query.title.toString();
+        res.send(products.filter(p => p.title.indexOf(searchString) > -1));
+    }
+    else {
+        res.send(products);
+    }
+});
+app.get('/products/:id', (req, res) => {
+    let product = products.find(p => p.id === +req.params.id);
+    if (product) {
+        res.send(product);
+    }
+    else {
+        res.send(404);
+    }
 });
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,28 @@ import express, { Request, Response } from 'express'
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req: Request, res: Response) => {
-	let helloMessage = 'Hello11 Wor ld!'
-	res.send(helloMessage)
+const products = [
+	{ id: 1, title: 'tomato' },
+	{ id: 2, title: 'orange' },
+]
+const adresses = [{ value: 'Nezalejnasti 12' }, { value: 'Selickaga 11' }]
+
+app.get('/products', (req: Request, res: Response) => {
+	if (req.query.title) {
+		let searchString = req.query.title.toString()
+		res.send(products.filter(p => p.title.indexOf(searchString) > -1))
+	} else {
+		res.send(products)
+	}
+})
+
+app.get('/products/:id', (req: Request, res: Response) => {
+	let product = products.find(p => p.id === +req.params.id)
+	if (product) {
+		res.send(product)
+	} else {
+		res.send(404)
+	}
 })
 
 app.listen(port, () => {
