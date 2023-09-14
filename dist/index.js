e9520bf751207708158944f1dbd93ffe17014506
Dzerdevwarnd <dzerdevwarnd3@gmail.com>
2023-09-14 17:56:51 +0300

    RestAPI

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const body_parser_1 = __importDefault(require("body-parser"));
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
@@ -11,6 +12,18 @@ const products = [
     { id: 2, title: 'orange' },
 ];
 const adresses = [{ value: 'Nezalejnasti 12' }, { value: 'Selickaga 11' }];
+const parserMiddleware = (0, body_parser_1.default)({});
+app.use(parserMiddleware);
+app.put('/products/:id', (req, res) => {
+    let product = products.find(p => p.id === +req.params.id);
+    if (product) {
+        product.title = req.body.title;
+        res.send(product);
+    }
+    else {
+        res.send(404);
+    }
+});
 app.get('/products', (req, res) => {
     if (req.query.title) {
         let searchString = req.query.title.toString();
@@ -28,6 +41,24 @@ app.get('/products/:id', (req, res) => {
     else {
         res.send(404);
     }
+});
+app.post('/products', (req, res) => {
+    const newProduct = {
+        id: +new Date(),
+        title: req.body.title,
+    };
+    products.push(newProduct);
+    res.status(201).send(newProduct);
+});
+app.delete('/products/:id', (req, res) => {
+    for (let i = 0; i < products.length; i++) {
+        if (products[i].id === +req.params.id) {
+            products.splice(i, 1);
+            res.send(204);
+            return;
+        }
+    }
+    res.send(404);
 });
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import bodyParser from 'body-parser'
 import express, { Request, Response } from 'express'
 const app = express()
 const port = process.env.PORT || 3000
@@ -7,6 +8,19 @@ const products = [
 	{ id: 2, title: 'orange' },
 ]
 const adresses = [{ value: 'Nezalejnasti 12' }, { value: 'Selickaga 11' }]
+
+const parserMiddleware = bodyParser({})
+app.use(parserMiddleware)
+
+app.put('/products/:id', (req: Request, res: Response) => {
+	let product = products.find(p => p.id === +req.params.id)
+	if (product) {
+		product.title = req.body.title
+		res.send(product)
+	} else {
+		res.send(404)
+	}
+})
 
 app.get('/products', (req: Request, res: Response) => {
 	if (req.query.title) {
@@ -24,6 +38,26 @@ app.get('/products/:id', (req: Request, res: Response) => {
 	} else {
 		res.send(404)
 	}
+})
+
+app.post('/products', (req: Request, res: Response) => {
+	const newProduct = {
+		id: +new Date(),
+		title: req.body.title,
+	}
+	products.push(newProduct)
+	res.status(201).send(newProduct)
+})
+
+app.delete('/products/:id', (req: Request, res: Response) => {
+	for (let i = 0; i < products.length; i++) {
+		if (products[i].id === +req.params.id) {
+			products.splice(i, 1)
+			res.send(204)
+			return
+		}
+	}
+	res.send(404)
 })
 
 app.listen(port, () => {
